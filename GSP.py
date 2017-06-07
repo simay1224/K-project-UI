4029b90d4834527c26dedbe80c151555ccdb1deb
dawnknight <atc327@nyu.edu>
2017-06-07 17:19:40 -0400

    add

--- GSP.py
+++ GSP.py
@@ -9,40 +9,31 @@ import cPickle
 import numpy as np
 from scipy import optimize
 
-src_path = 'I:/AllData_0327/unified data array/'
+#src_path = 'I:/AllData_0327/unified data array/'
+src_path = './data/unified data array/'
 Mfolder  = 'Unified_MData/'
 Kfolder  = 'Unified_KData/'
+Rfolder  = 'reliability/'
 
-#Mdata = {}
-#Kdata = {}
-#
-#Mdata[0] = cPickle.load(file(src_path+Mfolder+'Andy_2016-12-15 04.15.27 PM_ex4_FPS30_motion_unified.pkl','rb'))#[12:30,:]
-#Kdata[0] = cPickle.load(file(src_path+Kfolder+'Andy_data201612151615_unified_ex4.pkl','rb'))#[12:30,:]
-#
-#Jnum  = 6   # joint number
-#Tnum  = 2   # time interval
-#sigma = 1
-#
-#Len   = min(Mdata[0].shape[1],Kdata[0].shape[1])
-#Mdata[0] = Mdata[0][:,:Len].reshape((-1,3,Len))
-#Kdata[0] = Kdata[0][:,:Len].reshape((-1,3,Len))
-#
-#for i in range(1,Tnum):
-#    Mdata[i] = np.roll(Mdata[0],-i,axis = 2)[:,:,:-(tnum-1)]
-#    Kdata[i] = np.roll(Kdata[0],-i,axis = 2)[:,:,:-(tnum-1)]
-#    
-#Mdata[0] = Mdata[0][:,:,:-(Tnum-1)]
-#Kdata[0] = Kdata[0][:,:,:-(Tnum-1)]    
 
 Mdata = cPickle.load(file(src_path+Mfolder+'Andy_2016-12-15 04.15.27 PM_ex4_FPS30_motion_unified.pkl','rb'))[12:30,:]
 Kdata = cPickle.load(file(src_path+Kfolder+'Andy_data201612151615_unified_ex4.pkl','rb'))[12:30,:]
-Jnum  = 6   # joint number
-Tnum  = 2   # time interval
-sigma = 20
+Rdata = cPickle.load(file(src_path+Rfolder+'Andy_data201612151615_Rel_ex4.pkl','rb'))[4:10]
+
+Jnum   = 6   # joint number
+Tnum   = 2   # time interval
+sigma  = 20
+Rel_th = 0.7
+
 
 Len   = min(Mdata.shape[1],Kdata.shape[1])
 Mdata = Mdata[:,:Len].reshape((-1,3,Len))
 Kdata = Kdata[:,:Len].reshape((-1,3,Len))
+Rdata = Rdata[:,:Len]
+
+Kv    = Kdata - np.roll(Kdata,1,axis = 2) 
+Ka    = Kv - np.roll(Kv,1,axis = 2) 
+
 
 M = Mdata[:,:,:-(Tnum-1)]
 K = Kdata[:,:,:-(Tnum-1)]
@@ -54,7 +45,7 @@ for i in range(1,Tnum):
 adjmtx = np.zeros((Jnum*Tnum,Jnum*Tnum))
 degmtx = np.zeros((Jnum*Tnum,Jnum*Tnum))
 Lapmtx = np.zeros((Jnum*Tnum,Jnum*Tnum))
-
+L      = np.zeros((Jnum*Tnum))
 Cidx = np.arange(Jnum*Tnum)  #column idx
 Ridx = np.arange(Jnum*Tnum)  #row idx
 
@@ -74,19 +65,58 @@ for i in range(Jnum*Tnum):
     degmtx[i,i] = sum(adjmtx[i,:])
 #    print  degmtx[i,i]   
        
-Lapmtx[:] = degmtx - adjmtx
-
+Lapmtx[:] = degmtx - adjmtx 
 
 #F(x) = (X-Y).T*W*(X-Y)+X.T*L*X
 
 
+def func(X,Y):
+    x = X.reshape(12,3)
+    y = Y.reshape(12,3)
+    return sum(x-y)
 
-def func(X,u):
-    X = X.reshpe((18,-1)) 
-    return (X-Y).T*W*(X-Y)+u*X.T*L*X
+def func(X,u,Y,R,L):
+
     
     
-x =  optimize.fmin(func, np.random.rand(Jnum*3*Tnum),arg = (0.1) )   
+    return   (np.matmul(np.matmul((X[:,0]-Y[:,0]).T,np.diag(R)), (X[:,0]-Y[:,0]))**2+\
+              np.matmul(np.matmul((X[:,1]-Y[:,1]).T,np.diag(R)), (X[:,1]-Y[:,1]))**2+\
+              np.matmul(np.matmul((X[:,2]-Y[:,2]).T,np.diag(R)), (X[:,2]-Y[:,2]))**2)**0.5+\
+          u*((np.matmul(np.matmul(X[:,0].T,L),X[:,0])**2+\
+              np.matmul(np.matmul(X[:,1].T,L),X[:,1])**2+\
+              np.matmul(np.matmul(X[:,2].T,L),X[:,2])**2)**0.5)
+
+def pos_est(fidx,Kv,Ka,Kdata):
+    return Kdata[:,:,fidx]+Kv[:,:,fidx]+Ka[:,:,fidx]    
+          
+ 
+          
+relidx = np.where(np.sum((Rdata<Rel_th)*1,0)!=0)[0]   # frames which have unreliable  joints      
+relidx2 = np.where(np.sum((Rdata<Rel_th)*1,0)!=0)[0] 
+
+
+ 
+
+        
+for frame_idx in  relidx:
+    Y   = np.vstack([Kdata[:,:,frame_idx-1],Kdata[:,:,frame_idx]])
+    Rel = np.hstack([Rdata[:,frame_idx-1],Rdata[:,frame_idx]])
+    
+#    if frame_idx-1 in  relidx: 
+#        Xtm1_init = 
+#    else:
+    Xtm1_init = Kdata[:,:,frame_idx-1]
+    Xt_init   = pos_est(frame_idx,Kv,Ka,Kdata)
+
+    X_init = np.vstack([Xtm1_init,Xt_init])
+         
+          
+    x =  optimize.fmin(func, X_init ,args = (0.001,Y,Rel,Lapmtx,) ) 
+    
+    
+x =  optimize.fmin(func, X_init ,args = (Y,) )
+
+  
     
 
 
@@ -98,16 +128,18 @@ x =  optimize.fmin(func, np.random.rand(Jnum*3*Tnum),arg = (0.1) )
 
 
 
-#y = np.array([1,2])
-#def func(x):
-#    
-#    
-#    return sum((x-y)**2)
-#    
 #
-#w = optimize.fmin(func, np.random.rand(2) )
-#
-#print w
+from scipy import optimize
+import numpy as np
+def func(x,Y):
+    
+    a = x[0]-Y[0]
+    return sum(a**2)
+    
+y = np.array([1,2])
+w = optimize.fmin(func, x0 = np.random.rand(2),args =(y,))
+
+print w
 #        
 #    
 #from scipy import optimize
